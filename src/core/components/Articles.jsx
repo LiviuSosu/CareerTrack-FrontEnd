e7369287dc513b1de7247398d3b3c50b629fff28
LiviuSosu <sosuliviu@gmail.com>
2020-09-26 12:26:54 +0200

    added login component - not yet inetgrated with the server

---
+++ src/core/components/Articles.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Articles extends Component {
+    render() {
+        return (<div>
+            <p>this is the articles page</p>
+        </div>);
+    }
+}
