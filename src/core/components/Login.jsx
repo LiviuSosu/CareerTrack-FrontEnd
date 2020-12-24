a91bcf30f913d738e594b9ca57695d5cc7832524
LiviuSosu <sosuliviu@gmail.com>
2020-12-24 15:39:17 +0100

    remaking the login

--- src/core/components/Login.jsx
+++ src/core/components/Login.jsx
@@ -3,30 +3,38 @@ import React, { Component } from 'react';
 export class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {username: '', password: ''};
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value });
+  handleUsernameChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
   }
 
   handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.username + ' '+this.state.password);
+    event.preventDefault();
   }
 
     render() {
         return (
-            <form>
-            <label>
-              Name:
-              <input type="text" name="name" />
-            </label>
-            <label>
-              Password:
-              <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" onChange={this.handleChange}/>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Name:
+                <input type="text" value={this.state.username} onChange={this.handleUsernameChange}/>
+              </label>
+              <label>
+                Password:
+                <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+              </label>
+              <input type="submit" value="Submit"/>
           </form>
 
         );
