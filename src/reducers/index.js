b5cf194727d3dcda440800fc219040d7e9b63ab3
LiviuSosu <sosuliviu@gmail.com>
2021-01-16 23:18:49 +0100

    Updated the redux architecture. Replaced sagas with thunk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,43 +1,54 @@
-import { DATA_LOADED, LOGIN_SUCCEDED, LOGOUT_FAILED, LOGOUT_SUCCEDED } from "../constants/action-types";
+import { combineReducers } from 'redux';
 
-const initialState = {
-  articles: [],
-  remoteArticles: [],
-  loginResponse: {},
-  logoutResponse: {}
-};
+import { authentication } from './authentication.reducer';
 
-function rootReducer(state = initialState, action) {
-  // if (action.type === ADD_ARTICLE) {
-  //   return Object.assign({}, state, {
-  //     articles: state.articles.concat(action.payload)
-  //   });
-  // }
-
-  if (action.type === LOGOUT_SUCCEDED) {
-    return Object.assign({}, state, {
-      logoutResponse: action.payload
-    });
-  }
-
-  if (action.type === LOGIN_SUCCEDED) {
-    return Object.assign({}, state, {
-      loginResponse: action.payload
-    });
-  }
-
-  if (action.type === LOGOUT_FAILED) {
-    return Object.assign({}, state, {
-      loginResponse: "login failed"
-    });
-  }
-
-  if (action.type === DATA_LOADED) {
-    return Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat(action.payload)
-    });
-  }
-  return state;
-}
+const rootReducer = combineReducers({
+  authentication
+});
 
 export default rootReducer;
+
+
+// import { DATA_LOADED, LOGIN_SUCCEDED, LOGOUT_FAILED, LOGOUT_SUCCEDED } from "../constants/action-types";
+
+// const initialState = {
+//   articles: [],
+//   remoteArticles: [],
+//   loginResponse: {},
+//   logoutResponse: {}
+// };
+
+// function rootReducer(state = initialState, action) {
+//   // if (action.type === ADD_ARTICLE) {
+//   //   return Object.assign({}, state, {
+//   //     articles: state.articles.concat(action.payload)
+//   //   });
+//   // }
+
+//   if (action.type === LOGOUT_SUCCEDED) {
+//     return Object.assign({}, state, {
+//       logoutResponse: action.payload
+//     });
+//   }
+
+//   if (action.type === LOGIN_SUCCEDED) {
+//     return Object.assign({}, state, {
+//       loginResponse: action.payload
+//     });
+//   }
+
+//   if (action.type === LOGOUT_FAILED) {
+//     return Object.assign({}, state, {
+//       loginResponse: "login failed"
+//     });
+//   }
+
+//   if (action.type === DATA_LOADED) {
+//     return Object.assign({}, state, {
+//       remoteArticles: state.remoteArticles.concat(action.payload)
+//     });
+//   }
+//   return state;
+// }
+
+// export default rootReducer;
